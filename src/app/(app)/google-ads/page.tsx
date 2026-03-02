c606c3e5e0ef93c42cd249b20dfbeae96149b569
Jason Yu <yj2008ay611@gmail.com>
2026-03-02 15:52:41 +0800

    feat(google-ads): add 100 page size option for account list

--- src/app/(app)/google-ads/page.tsx
+++ src/app/(app)/google-ads/page.tsx
@@ -911,6 +911,7 @@ export default function GoogleAdsPage() {
                             <option value={10}>10</option>
                             <option value={20}>20</option>
                             <option value={50}>50</option>
+                            <option value={100}>100</option>
                           </select>
                           <span>条</span>
                         </div>
